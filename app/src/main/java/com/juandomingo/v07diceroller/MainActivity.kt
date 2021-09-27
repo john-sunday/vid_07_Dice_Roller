55efa642d097d5ba8f13024fc31daadda9b14b51
juand <domingoortin.j@gmail.com>
2021-09-27 11:23:46 +0200

    1CM. Project Dice Roller Commit

---
+++ app/src/main/java/com/juandomingo/v07diceroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.juandomingo.v07diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
